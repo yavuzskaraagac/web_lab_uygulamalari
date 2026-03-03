fc4eed205d0c5720f6d141e97c2e5ad17626871c
yavuzskaraagac <yavuzkaraagac8@gmail.com>
2026-03-03 11:07:12 +0300

    chore: initial project setup with Vite + React + TS

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Web Tasarimi ve Programlama</h1>
+      <h2>LAB-1</h2>
+      <p>Ad Soyad: Yavuz Selim Karaağaç</p>
+      <p>Ogrenci No: 235542017</p>
+    </div>
+  )
+}
+
+export default App
